ad280a4c1787977cd5cab0f66cebb7a7891cffa6
Rot4tion <nhonhoem0123@gmail.com>
2025-02-20 11:27:04 +0700

    Updates role checking to handle development environment
    Modifies the role verification logic to return user ID matches only in the development environment, enhancing error handling for different environments.
    
    Returns false for non-development environments to prevent unintended access.
    
    Improves code clarity and maintains security across environments.

--- src/collections/Users/access/hasRoleOrSelf.ts
+++ src/collections/Users/access/hasRoleOrSelf.ts
@@ -5,7 +5,10 @@ const hasRoleOrSelf =
   (roles: User['roles']) =>
   ({ req }: { req: PayloadRequest } | AccessArgs): boolean | Promise<boolean> => {
     if (hasRole(roles)({ req })) return true
-    //@ts-expect-error ts missmatch
-    return { id: { equals: req.user?.id } }
+    if (process.env.NODE_ENV === 'development') {
+      //@ts-expect-error ts missmatch
+      return { id: { equals: req.user?.id } }
+    }
+    return false
   }
 export default hasRoleOrSelf
