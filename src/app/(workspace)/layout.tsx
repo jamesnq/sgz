bc6b37a804280b2506464050a0e00006b1cf1d8b
Rot4tion <nhonhoem0123@gmail.com>
2025-02-28 20:24:14 +0700

    add workspace

---
+++ src/app/(workspace)/layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { Providers } from '@/providers'
+import { InitTheme } from '@/providers/Theme/InitTheme'
+
+import '../(frontend)/globals.css'
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <InitTheme />
+        <link href="/favicon.ico" rel="icon" sizes="32x32" />
+        <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+      </head>
+      <body className="antialiased">
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  )
+}
