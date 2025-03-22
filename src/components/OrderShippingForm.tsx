fea96db4375c07089ca6c64ea5e4ff8463b480ee
Rot4tion <nhonhoem0123@gmail.com>
2025-03-22 22:27:38 +0700

    Updates lucide-react dependency and enhances form functionality
    Upgrades the lucide-react package to version 0.483.0 for improved icon support.
    
    Enhances the OrderShippingForm component by introducing a loading state for the update action, allowing for better user feedback during asynchronous operations. Refactors the update logic to streamline the order status updates and improve code clarity.
    
    Relates to better user experience and maintainability.

--- src/components/OrderShippingForm.tsx
+++ src/components/OrderShippingForm.tsx
@@ -5,10 +5,9 @@ import { fields } from '@/blocks/Form/fields'
 import { Form, FormSubmission, Order } from '@/payload-types'
 import payloadClient from '@/utilities/payloadClient'
 import { cn } from '@/utilities/ui'
-import { Check, Copy, Pencil } from 'lucide-react'
+import { Check, Copy, Loader2, UserPen } from 'lucide-react'
 import { useMemo, useState } from 'react'
-
-// import payloadClient from '@/utilities/payloadClient'
+import { useMutation } from '@tanstack/react-query'
 
 interface OrderShippingFormProps {
   order: Order
@@ -20,6 +19,7 @@ export function OrderShippingForm({ order }: OrderShippingFormProps) {
   const form = useMemo(() => formSubmission?.form as Form, [formSubmission])
   const submissionData = useMemo(() => formSubmission?.submissionData || {}, [formSubmission])
   const [copiedField, setCopiedField] = useState<string | null>(null)
+  const [updatingField, setUpdatingField] = useState<string | null>(null)
 
   const handleCopy = (value: string, fieldName: string) => {
     if (!value) return
@@ -29,23 +29,31 @@ export function OrderShippingForm({ order }: OrderShippingFormProps) {
       setCopiedField(null)
     }, 1500)
   }
-  const updateNeedProcessRequired = async (fieldName: string) => {
-    await payloadClient.updateById({
-      collection: 'form-submissions',
-      id: formSubmission.id,
-      data: { submissionData: { ...(submissionData as any), [fieldName]: {} } },
-    })
-    if (order.status !== 'USER_UPDATE') {
+
+  const { mutate: updateNeedProcessRequiredMutation, isPending } = useMutation({
+    mutationFn: async ({ fieldName }: { fieldName: string }) => {
+      setUpdatingField(fieldName)
       await payloadClient.updateById({
-        collection: 'orders',
-        id: order.id,
-        data: { status: 'USER_UPDATE' },
+        collection: 'form-submissions',
+        id: formSubmission.id,
+        data: { submissionData: { ...(submissionData as any), [fieldName]: {} } },
       })
-    }
-    await refetch()
-  }
+
+      if (order.status !== 'USER_UPDATE') {
+        await payloadClient.updateById({
+          collection: 'orders',
+          id: order.id,
+          data: { status: 'USER_UPDATE' },
+        })
+      }
+
+      await refetch()
+      setUpdatingField(null)
+    },
+  })
+
   return (
-    <div className="grid grid-cols-2 gap-2">
+    <div className="grid grid-cols-2 gap-2 mt-1">
       {form?.fields?.map((field, index) => {
         const Field: React.FC<any> = fields?.[field.blockType as keyof typeof fields]
         if (!Field) return null
@@ -54,57 +62,63 @@ export function OrderShippingForm({ order }: OrderShippingFormProps) {
         const isCopied = copiedField === field.name
         const isProcessRequired = typeof value === 'object'
         const isHaveValue = value && !isProcessRequired
+        const isUpdating = updatingField === field.name
+
         return (
-          <div
-            key={index}
-            onClick={() => isHaveValue && handleCopy(value, field.name)}
-            className={cn(
-              'relative rounded-md border px-2 py-1.5 transition-all',
-              !value && 'border-dashed opacity-70',
-              value && 'cursor-pointer hover:bg-muted/50 active:bg-muted',
-              isCopied && 'ring-1 ring-green-500',
-            )}
-          >
-            <div className="flex items-center gap-0.5 text-xs font-medium text-muted-foreground">
-              {field.label}
-              {field.required && <span className="text-destructive">*</span>}
-            </div>
-            <div className="flex items-center justify-between gap-1">
-              <div className="truncate text-sm">
-                {isHaveValue ? (
-                  value
-                ) : (
-                  <span className="text-muted-foreground italic">Chưa có</span>
-                )}
+          <div key={index} className="w-full flex">
+            <div
+              onClick={() => isHaveValue && handleCopy(value, field.name)}
+              className={cn(
+                'w-[90%] relative rounded-md border px-2 py-1.5 transition-all',
+                !value && 'border-dashed opacity-70',
+                value && 'cursor-pointer hover:bg-muted/50 active:bg-muted',
+                isCopied && 'ring-1 ring-green-500',
+              )}
+            >
+              <div className="flex items-center gap-0.5 text-xs font-medium text-muted-foreground">
+                {field.label}
+                {field.required && <span className="text-destructive">*</span>}
               </div>
-              <div className="flex items-center gap-1">
-                {isHaveValue && (
-                  <div
-                    className={cn(
-                      'flex h-4 w-4 shrink-0 items-center justify-center text-muted-foreground transition-colors',
-                      isCopied && 'text-green-500',
-                    )}
-                  >
-                    {isCopied ? (
-                      <Check className="h-3 w-3" />
-                    ) : (
-                      value && <Copy className="h-3 w-3" />
-                    )}
-                  </div>
-                )}
-                <button
-                  className={cn(
-                    'flex h-6 w-6 shrink-0 items-center justify-center rounded-full text-muted-foreground transition-colors hover:bg-muted hover:text-highlight',
-                    isProcessRequired && 'text-highlight',
+              <div className="flex items-center justify-between gap-1">
+                <div className="truncate text-sm">
+                  {isHaveValue ? (
+                    value
+                  ) : (
+                    <span className="text-muted-foreground italic">Chưa có</span>
                   )}
-                  onClick={async () => {
-                    await updateNeedProcessRequired(field.name)
-                  }}
-                >
-                  <Pencil className="h-3 w-3" />
-                </button>
+                </div>
+                <div className="flex items-center gap-1">
+                  {isHaveValue && (
+                    <div
+                      className={cn(
+                        'flex h-4 w-4 shrink-0 items-center justify-center text-muted-foreground transition-colors',
+                        isCopied && 'text-green-500',
+                      )}
+                    >
+                      {isCopied ? (
+                        <Check className="h-3 w-3" />
+                      ) : (
+                        value && <Copy className="h-3 w-3" />
+                      )}
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
+            <button
+              className={cn(
+                'w-[10%] flex border shrink-0 items-center justify-center text-muted-foreground transition-colors hover:bg-muted hover:text-highlight',
+                isProcessRequired && 'text-highlight',
+              )}
+              onClick={() => updateNeedProcessRequiredMutation({ fieldName: field.name })}
+              disabled={isPending && isUpdating}
+            >
+              {isUpdating ? (
+                <Loader2 className="h-4 w-4 animate-spin" />
+              ) : (
+                <UserPen className="h-5 w-5" />
+              )}
+            </button>
           </div>
         )
       })}
