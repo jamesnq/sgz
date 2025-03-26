9b25b3da324c5f943235d98aa6ded5f3c897b76c
Rot4tion <nhonhoem0123@gmail.com>
2025-03-26 08:45:46 +0700

    Adds Plausible analytics integration
    Integrates `next-plausible` for enhanced analytics tracking.
    
    Wraps the application in a `PlausibleProvider` to enable tracking of file downloads, outbound links, and tagged events. This addition aims to improve user insights and engagement monitoring.
    
    Updates package dependencies accordingly.

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -7,22 +7,33 @@ import { ClientProviders } from './client-providers'
 import { HeaderThemeProvider } from './HeaderTheme'
 import { ReactQueryProvider } from './react-query-provider'
 import { ThemeProvider } from './Theme'
+import PlausibleProvider from 'next-plausible'
 
 export const Providers: React.FC<{
   children: React.ReactNode
 }> = ({ children }) => {
   return (
-    <ReactQueryProvider>
-      <AuthProvider>
-        <NuqsAdapter>
-          <ThemeProvider>
-            <HeaderThemeProvider>
-              <ClientProviders>{children}</ClientProviders>
-              <ChatwootLoader />
-            </HeaderThemeProvider>
-          </ThemeProvider>
-        </NuqsAdapter>
-      </AuthProvider>
-    </ReactQueryProvider>
+    <PlausibleProvider
+      selfHosted
+      customDomain="https://plausible.subgamezone.com/"
+      domain={process.env.NEXT_PUBLIC_SERVER_URL}
+      enabled
+      trackFileDownloads
+      trackOutboundLinks
+      taggedEvents
+    >
+      <ReactQueryProvider>
+        <AuthProvider>
+          <NuqsAdapter>
+            <ThemeProvider>
+              <HeaderThemeProvider>
+                <ClientProviders>{children}</ClientProviders>
+                <ChatwootLoader />
+              </HeaderThemeProvider>
+            </ThemeProvider>
+          </NuqsAdapter>
+        </AuthProvider>
+      </ReactQueryProvider>
+    </PlausibleProvider>
   )
 }
