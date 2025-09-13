9dbd6b982751990d8ad7fe6bfe96d7b961d94d08
Rot4tion <nhonhoem0123@gmail.com>
2025-09-13 16:27:39 +0700

    test add chatwoot notify to discord

---
+++ src/app/(payload)/api/webhook/chatwoot/route.ts
@@ -0,0 +1,8 @@
+import { discordWebhook } from '@/services/novu.service'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function POST(request: NextRequest) {
+  const body = await request.json()
+  await discordWebhook({ subject: 'Need Support', message: JSON.stringify(body, null, 2) })
+  return NextResponse.json({ message: 'ok' })
+}
