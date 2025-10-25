8b020180ae53035376b7de54910ae24909661130
Rot4tion <nhonhoem0123@gmail.com>
2025-10-25 16:25:33 +0700

    chore: temporarily disable chatwoot webhook discord notifications

--- src/app/(payload)/api/webhook/chatwoot/route.ts
+++ src/app/(payload)/api/webhook/chatwoot/route.ts
@@ -2,7 +2,7 @@ import { discordWebhook } from '@/services/novu.service'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(request: NextRequest) {
-  const body = await request.json()
-  await discordWebhook({ subject: 'Need Support', message: JSON.stringify(body, null, 2) })
+  // const body = await request.json()
+  // await discordWebhook({ subject: 'Need Support', message: JSON.stringify(body, null, 2) })
   return NextResponse.json({ message: 'ok' })
 }
